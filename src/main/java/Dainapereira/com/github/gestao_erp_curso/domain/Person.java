8f0f1139b5de58fc41019173d7de7f4565f1b4f4
Daina Pereira <daina.pereira36@gmail.com>
2025-01-13 22:15:31 -0300

    Criacao da classe de pessoa e correcao do yml

---
+++ src/main/java/Dainapereira/com/github/gestao_erp_curso/domain/Person.java
@@ -0,0 +1,19 @@
+package Dainapereira.com.github.gestao_erp_curso.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="person")
+public class Person {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  private String name;
+
+}
